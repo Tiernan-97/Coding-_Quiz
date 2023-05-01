98a8b378dd2de572bd162defe168e415963537af
Tiernan Green <tiernanjamesgreen@gmail.com>
2023-05-01 23:27:30 +0100

    Created a working quiz, without any way to save your score

--- assets/JS/quiz.js
+++ assets/JS/quiz.js
@@ -21,16 +21,21 @@ function endGame() {
 
     quiz.style.display = 'none';
     results.style.display = 'flex';
+
+    results.textContent = "Your score is: " + score;
 };
 
 function startGame() {
-    secondsLeft = 60;
+    secondsLeft = 75;
 
-    currentQuestion = 0;
+    currentQuestion = -1;
 
-    countdownTimer = setInterval(function (){
+    score = 0;
+
+    countdownTimer = setInterval(function () {
+
         if (secondsLeft>0) {
-            timer.textcontent = secondsLeft;
+            timer.textContent = secondsLeft;
         } else {
             endGame();
         }
@@ -44,11 +49,56 @@ function startGame() {
     displayQuestion();
 }
 
-function displayQuestion() {
-    currentQuestion++;
+function displayQuestion() {;
+    currentQuestion++
+
 
     if (currentQuestion >= questions.length) {
         endGame();
         return
     }
+
+    var question = questions[currentQuestion];
+    document.getElementById("title").textContent = question.title;
+
+    answers.innerHTML = "";
+
+    for (var i = 0; i<question.choices.length; i++) {
+
+        var answer = document.createElement("div");
+        answer.textContent = question.choices[i];
+        answer.onclick = onSelectAnswer;
+        answer.classList.add("answer");
+
+        answers.appendChild(answer);
+    }
+
 }
+
+function onSelectAnswer(e) {
+    var correctAnswer = questions[currentQuestion].answer;
+
+    var userAnswer = e.target.textContent;
+    if (correctAnswer === userAnswer) {
+        score++;
+
+        displayMessage('Correct!');
+    } else {
+        score--;
+        secondsLeft=secondsLeft-5;
+        displayMessage('Incorrect! You lost 5 seconds!');
+    }
+
+    displayQuestion();
+}
+
+
+function displayMessage(msg) {
+    message.textContent = msg;
+
+    setTimeout(function () {
+        message.textContent = "";
+    }, 1000);
+}
+
+quizStart.addEventListener("click", startGame);
